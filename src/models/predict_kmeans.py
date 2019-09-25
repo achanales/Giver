0f855c1c6483f8f95cd10bdc34af7d6888c9b9ed
Avi Chanales <avi.chanales@gmail.com>
2019-09-25 17:11:46 -0400

    added lambda function

--- src/models/predict_kmeans.py
+++
@@ -1,7 +0,0 @@
-from sklearn.cluster import KMeans
-
-def predict_kmeans(model,doc_emb):
-    
-    prediction = model.predict(art_emb)
-    
-    return prediction
--- src/models/run_kmeans.py
+++
@@ -1,8 +0,0 @@
-from sklearn.cluster import KMeans
-
-def run_kmeans(n_clusters,docs_emb,max_iter = 500, n_init=15):
-    
-    model = KMeans(n_clusters=true_k, init='k-means++', max_iter=max_iter, n_init=n_init)
-    model.fit(charity_docs_emb)
-    
-    return model
